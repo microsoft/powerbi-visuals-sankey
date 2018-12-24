9f8fd99ab3d84eef0f2230b075c406db3923835e
Ilfat Galiev <zbritva@gmail.com>
2018-12-25 01:08:15 +0300

    Webpack based visual
    update d3 to v5
    replace lodash by lodas-es
    update all utils to the latest
    update the visual API to 2.3.0

--- src/behavior.ts
+++ src/behavior.ts
@@ -1,125 +1,137 @@
 /*
- *  Power BI Visualizations
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
+*  Power BI Visualizations
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
 
-module powerbi.extensibility.visual {
-    // d3
-    import Selection = d3.Selection;
+import * as d3 from "d3";
 
-    // powerbi.extensibility.utils.interactivity
-    import IInteractivityService = powerbi.extensibility.utils.interactivity.IInteractivityService;
-    import IInteractiveBehavior = powerbi.extensibility.utils.interactivity.IInteractiveBehavior;
-    import ISelectionHandler = powerbi.extensibility.utils.interactivity.ISelectionHandler;
-    import SelectableDataPoint = powerbi.extensibility.utils.interactivity.SelectableDataPoint;
+const getEvent = (): MouseEvent => require("d3-selection").event as MouseEvent;
 
-    export interface SankeyDiagramBehaviorOptions {
-        nodes: Selection<SankeyDiagramNode>;
-        links: Selection<SankeyDiagramLink>;
-        clearCatcher: Selection<any>;
-        interactivityService: IInteractivityService;
+import {
+    SankeyDiagramLink,
+    SankeyDiagramNode
+} from "./dataInterfaces";
+
+import * as sankeyDiagramUtils from "./utils";
+
+// d3
+type Selection<T> = d3.Selection<any, T, any, any>;
+
+// powerbi.extensibility.utils.interactivity
+import { interactivitySelectionService, interactivityBaseService } from "powerbi-visuals-utils-interactivityutils";
+import SelectableDataPoint = interactivitySelectionService.SelectableDataPoint;
+import IInteractiveBehavior = interactivityBaseService.IInteractiveBehavior;
+import IInteractivityService = interactivityBaseService.IInteractivityService;
+
+import ISelectionHandler = interactivityBaseService.ISelectionHandler;
+import IBehaviorOptions = interactivityBaseService.IBehaviorOptions;
+
+export interface SankeyDiagramBehaviorOptions extends IBehaviorOptions<SelectableDataPoint> {
+    nodes: Selection<SankeyDiagramNode>;
+    links: Selection<SankeyDiagramLink>;
+    clearCatcher: Selection<any>;
+    interactivityService: IInteractivityService<SelectableDataPoint>;
+}
+
+export class SankeyDiagramBehavior implements IInteractiveBehavior {
+    private behaviorOptions: SankeyDiagramBehaviorOptions;
+    private selectionHandler: ISelectionHandler;
+
+    private selectedDataPoints: SelectableDataPoint[];
+
+    public static create(): IInteractiveBehavior {
+        return new SankeyDiagramBehavior();
     }
 
-    export class SankeyDiagramBehavior implements IInteractiveBehavior {
-        private behaviorOptions: SankeyDiagramBehaviorOptions;
-        private selectionHandler: ISelectionHandler;
+    constructor() {
+        this.createAnEmptySelectedDataPoints();
+    }
 
-        private selectedDataPoints: SelectableDataPoint[];
+    public bindEvents(
+        behaviorOptions: SankeyDiagramBehaviorOptions,
+        selectionHandler: ISelectionHandler): void {
 
-        public static create(): IInteractiveBehavior {
-            return new SankeyDiagramBehavior();
-        }
+        this.behaviorOptions = behaviorOptions;
+        this.selectionHandler = selectionHandler;
 
-        constructor() {
+        this.bindClickEventToNodes();
+        this.bindClickEventToLinks();
+        this.bindClickEventToClearCatcher();
+    }
+
+    private bindClickEventToNodes(): void {
+        this.behaviorOptions.nodes.on("click", (node: SankeyDiagramNode) => {
+            let selectableDataPoints: SelectableDataPoint[] = node.selectableDataPoints;
+            if (node.cloneLink) {
+                selectableDataPoints = selectableDataPoints.concat(node.cloneLink.selectableDataPoints);
+            }
+
+            this.clearSelection();
+
+            if (!sankeyDiagramUtils.areDataPointsSelected(this.selectedDataPoints, selectableDataPoints)) {
+                selectableDataPoints.forEach((subDataPoint: SelectableDataPoint) => {
+                    this.selectionHandler.handleSelection(subDataPoint, true);
+                });
+
+                this.selectedDataPoints = selectableDataPoints;
+            } else {
+                this.createAnEmptySelectedDataPoints();
+            }
+        });
+    }
+
+    private bindClickEventToLinks(): void {
+        this.behaviorOptions.links.on("click", (link: SankeyDiagramLink) => {
+            this.selectionHandler.handleSelection(link, getEvent().ctrlKey);
             this.createAnEmptySelectedDataPoints();
-        }
+        });
+    }
 
-        public bindEvents(
-            behaviorOptions: SankeyDiagramBehaviorOptions,
-            selectionHandler: ISelectionHandler): void {
+    private bindClickEventToClearCatcher(): void {
+        this.behaviorOptions.clearCatcher.on("click", () => {
+            this.clearSelection();
+            this.createAnEmptySelectedDataPoints();
+        });
+    }
 
-            this.behaviorOptions = behaviorOptions;
-            this.selectionHandler = selectionHandler;
+    private clearSelection(): void {
+        this.selectionHandler.handleClearSelection();
+    }
 
-            this.bindClickEventToNodes();
-            this.bindClickEventToLinks();
-            this.bindClickEventToClearCatcher();
-        }
+    private createAnEmptySelectedDataPoints(): void {
+        this.selectedDataPoints = [];
+    }
 
-        private bindClickEventToNodes(): void {
-            this.behaviorOptions.nodes.on("click", (node: SankeyDiagramNode) => {
-                let selectableDataPoints: SelectableDataPoint[] = node.selectableDataPoints;
-                if (node.cloneLink) {
-                    selectableDataPoints = selectableDataPoints.concat(node.cloneLink.selectableDataPoints);
-                }
+    public renderSelection(hasSelection: boolean): void {
+        sankeyDiagramUtils.updateFillOpacity(
+            this.behaviorOptions.links,
+            this.behaviorOptions.interactivityService,
+            hasSelection);
 
-                this.clearSelection();
-
-                if (!sankeyDiagramUtils.areDataPointsSelected(this.selectedDataPoints, selectableDataPoints)) {
-                    selectableDataPoints.forEach((subDataPoint: SelectableDataPoint) => {
-                        this.selectionHandler.handleSelection(subDataPoint, true);
-                    });
-
-                    this.selectedDataPoints = selectableDataPoints;
-                } else {
-                    this.createAnEmptySelectedDataPoints();
-                }
-            });
-        }
-
-        private bindClickEventToLinks(): void {
-            this.behaviorOptions.links.on("click", (link: SankeyDiagramLink) => {
-                this.selectionHandler.handleSelection(link, (d3.event as MouseEvent).ctrlKey);
-                this.createAnEmptySelectedDataPoints();
-            });
-        }
-
-        private bindClickEventToClearCatcher(): void {
-            this.behaviorOptions.clearCatcher.on("click", () => {
-                this.clearSelection();
-                this.createAnEmptySelectedDataPoints();
-            });
-        }
-
-        private clearSelection(): void {
-            this.selectionHandler.handleClearSelection();
-        }
-
-        private createAnEmptySelectedDataPoints(): void {
-            this.selectedDataPoints = [];
-        }
-
-        public renderSelection(hasSelection: boolean): void {
-            sankeyDiagramUtils.updateFillOpacity(
-                this.behaviorOptions.links,
-                this.behaviorOptions.interactivityService,
-                hasSelection);
-
-            sankeyDiagramUtils.updateFillOpacity(
-                this.behaviorOptions.nodes,
-                this.behaviorOptions.interactivityService,
-                hasSelection);
-        }
+        sankeyDiagramUtils.updateFillOpacity(
+            this.behaviorOptions.nodes,
+            this.behaviorOptions.interactivityService,
+            hasSelection);
     }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,89 +1,95 @@
 /*
- *  Power BI Visualizations
- *
- *  Copyright (c) Microsoft Corporation
- *  All rights reserved.
- *  MIT License
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the ""Software""), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
+*  Power BI Visualizations
+*
+*  Copyright (c) Microsoft Corporation
+*  All rights reserved.
+*  MIT License
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the ""Software""), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in
+*  all copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
 
-module powerbi.extensibility.visual {
-    import DataViewObjectsParser = powerbi.extensibility.utils.dataview.DataViewObjectsParser;
+import {
+    dataViewObjectsParser
+} from "powerbi-visuals-utils-dataviewutils";
 
-    export enum CyclesDrawType {
-        Duplicate,
-        Backward
-    }
+import {
+    SankeyDiagramNodePositionSetting
+} from "./dataInterfaces";
 
-    export class SankeyDiagramLabelsSettings {
-        public static DefaultFontSize: number = 12;
+import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
-        public show: boolean = true;
-        public fill: string = "black";
-        public fontSize: number = SankeyDiagramLabelsSettings.DefaultFontSize;
-        public forceDisplay: boolean = false;
-        public minHeightOfNode: number = 10;
-        public unit?: number = 0;
-    }
+export enum CyclesDrawType {
+    Duplicate,
+    Backward
+}
 
-    export class SankeyDiagramLinkLabelsSettings {
-        public static DefaultFontSize: number = 12;
-        public show: boolean = false;
-        public fill: string = "black";
-        public fontSize: number = SankeyDiagramLabelsSettings.DefaultFontSize;
-        public minHeightOfNode: number = 10;
-    }
+export class SankeyDiagramLabelsSettings {
+    public static DefaultFontSize: number = 12;
 
-    export class SankeyDiagramScaleSettings {
-        public x: number = 1;
-        public y: number = 1;
-    }
+    public show: boolean = true;
+    public fill: string = "black";
+    public fontSize: number = SankeyDiagramLabelsSettings.DefaultFontSize;
+    public forceDisplay: boolean = false;
+    public minHeightOfNode: number = 10;
+    public unit?: number = 0;
+}
 
-    export class SankeyScaleSettings {
-        public provideMinHeight: boolean = true;
-        public lnScale: boolean = false;
-    }
+export class SankeyDiagramLinkLabelsSettings {
+    public static DefaultFontSize: number = 12;
+    public show: boolean = false;
+    public fill: string = "black";
+    public fontSize: number = SankeyDiagramLabelsSettings.DefaultFontSize;
+    public minHeightOfNode: number = 10;
+}
 
-    export class SankeyComplexSettings {
-        public nodePositions: string = "[]";
-        public viewportSize: string = "{}";
-    }
+export class SankeyDiagramScaleSettings {
+    public x: number = 1;
+    public y: number = 1;
+}
 
-    export class SankeyNodeCycles {
-        public drawCycles: number = CyclesDrawType.Duplicate;
-        public selfLinksWeight: boolean = true;
-    }
+export class SankeyScaleSettings {
+    public provideMinHeight: boolean = true;
+    public lnScale: boolean = false;
+}
 
-    export interface ViewportSize {
-        height?: string;
-        width?: string;
-    }
+export class SankeyComplexSettings {
+    public nodePositions: string = "[]";
+    public viewportSize: string = "{}";
+}
 
-    export class SankeyDiagramSettings extends DataViewObjectsParser {
-        public linkLabels: SankeyDiagramLinkLabelsSettings = new SankeyDiagramLinkLabelsSettings();
-        public labels: SankeyDiagramLabelsSettings = new SankeyDiagramLabelsSettings();
-        public _scale: SankeyDiagramScaleSettings = new SankeyDiagramScaleSettings();
-        public scaleSettings: SankeyScaleSettings = new SankeyScaleSettings();
-        public nodeComplexSettings: SankeyComplexSettings = new SankeyComplexSettings();
-        public _nodePositions: SankeyDiagramNodePositionSetting[] = [];
-        public cyclesLinks: SankeyNodeCycles = new SankeyNodeCycles();
-        public _viewportSize: ViewportSize = {};
-    }
+export class SankeyNodeCycles {
+    public drawCycles: number = CyclesDrawType.Duplicate;
+    public selfLinksWeight: boolean = true;
+}
+
+export interface ViewportSize {
+    height?: string;
+    width?: string;
+}
+
+export class SankeyDiagramSettings extends DataViewObjectsParser {
+    public linkLabels: SankeyDiagramLinkLabelsSettings = new SankeyDiagramLinkLabelsSettings();
+    public labels: SankeyDiagramLabelsSettings = new SankeyDiagramLabelsSettings();
+    public _scale: SankeyDiagramScaleSettings = new SankeyDiagramScaleSettings();
+    public scaleSettings: SankeyScaleSettings = new SankeyScaleSettings();
+    public nodeComplexSettings: SankeyComplexSettings = new SankeyComplexSettings();
+    public _nodePositions: SankeyDiagramNodePositionSetting[] = [];
+    public cyclesLinks: SankeyNodeCycles = new SankeyNodeCycles();
+    public _viewportSize: ViewportSize = {};
 }
