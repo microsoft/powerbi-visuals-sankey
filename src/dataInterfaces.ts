73601d8ac35477836536277dece241eeff13b2d3
kullJul <86924383+kullJul@users.noreply.github.com>
2025-07-02 17:13:04 +0200

    Fix tests

--- src/dataInterfaces.ts
+++ src/dataInterfaces.ts
@@ -117,7 +117,7 @@ export interface SankeyDiagramLink extends
 export interface SankeyDiagramColumn {
     countOfNodes: number;
     sumValueOfNodes: number;
-    x: number;
+    x?: number;
 }
 
 export enum SankeyDiagramNodeStatus {
